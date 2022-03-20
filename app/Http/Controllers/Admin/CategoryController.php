2b9e2cc7eec4d3e251844e3b2169fa0f30dfafb7
Imran189 <imranimu189@gmail.com>
2022-03-20 21:50:02 +0600

    third commit

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 
 class CategoryController extends Controller
 {
     public function index(){
-        return view('admin.categories.categories');
+        $categories = Category::all();
+        return view('admin.categories.categories', compact('categories'));
     }
 }
